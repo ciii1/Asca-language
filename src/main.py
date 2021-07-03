ac6521de51d15dda491327db3c7f483e9061a96e
blend-smile <blend-smile@protonmail.com>
2021-07-03 07:08:42 +0000

    fix for generator on input from file

--- src/main.py
+++ src/main.py
@@ -7,59 +7,61 @@ import sys
 
 def init_tokens(input):
     token_exprs = [
-        (r'\n',                                            None),
-        (r'[ \t]+',                                        None),
-        (r'#[^\n]*',                                       None),
-        (r'\[',                                            'RESERVED'),
-        (r'\]',                                            'RESERVED'),
-        (r'\:',                                            'RESERVED'),
-        (r'\;',                                            'RESERVED'),
-        (r',',                                             'RESERVED'),
-        (r'\(',                                            'RESERVED'),
-        (r'\)',                                            'RESERVED'),
-        (r'\{',                                            'RESERVED'),
-        (r'\}',                                            'RESERVED'),
-        (r'@',                                             'UNARY_OPERATOR'),
-        (r'\$',                                            'UNARY_OPERATOR'),
-        (r'!',                                             'UNARY_OPERATOR'),
-        (r'==',                                            'RELATIONAL_OPERATOR'),
-        (r'<=',                                            'RELATIONAL_OPERATOR'),
-        (r'<',                                             'RELATIONAL_OPERATOR'),
-        (r'>=',                                            'RELATIONAL_OPERATOR'),
-        (r'>',                                             'RELATIONAL_OPERATOR'),
-        (r'!=',                                            'RELATIONAL_OPERATOR'),
-        (r'&&',                                            'CONDITIONAL_OPERATOR'),
-        (r'\|\|',                                          'CONDITIONAL_OPERATOR'),
-        (r'=',                                             'ASSIGNMENT_OPERATOR'),
-        (r'\+=',                                           'ASSIGNMENT_OPERATOR'),
-        (r'\-=',                                           'ASSIGNMENT_OPERATOR'),
-        (r'\/=',                                           'ASSIGNMENT_OPERATOR'),
-        (r'\*=',                                           'ASSIGNMENT_OPERATOR'),
-        (r'\+',                                            'ARITHMETICAL_OPERATOR'),
-        (r'-',                                             'ARITHMETICAL_OPERATOR'),
-        (r'\*',                                            'ARITHMETICAL_OPERATOR'),
-        (r'/',                                             'ARITHMETICAL_OPERATOR'),
-        (r'(?![A-Za-z0-9_])type(?![A-Za-z0-9_])',          'RESERVED'),
-        (r'(?![A-Za-z0-9_])func(?![A-Za-z0-9_])',          'RESERVED'),
-        (r'(?![A-Za-z0-9_])if(?![A-Za-z0-9_])',            'RESERVED'),
-        (r'(?![A-Za-z0-9_])elif(?![A-Za-z0-9_])',          'RESERVED'),
-        (r'(?![A-Za-z0-9_])else(?![A-Za-z0-9_])',          'RESERVED'),
-        (r'(?![A-Za-z0-9_])while(?![A-Za-z0-9_])',         'RESERVED'),
-        (r'(?![A-Za-z0-9_])for(?![A-Za-z0-9_])',           'RESERVED'),
-        (r'(?![A-Za-z0-9_])break(?![A-Za-z0-9_])',         'RESERVED'),
-        (r'(?![A-Za-z0-9_])continue(?![A-Za-z0-9_])',      'RESERVED'),
-        (r'(?![A-Za-z0-9_])return(?![A-Za-z0-9_])',        'RESERVED'),
-        (r'\".*?\"',                            '           STRING'),
-        (r'[0-9]+\.[0-9]+',                     '           FLOAT'),
-        (r'\'.*?\'',                         '              CHAR'),
-        (r"""(?![A-Za-z0-9_])true(?![A-Za-z0-9_])|
-          (?![A-Za-z0-9_])false(?![A-Za-z0-9_])""",         'BOOL'),
-        (r'(?![A-Za-z0-9_])qword(?![A-Za-z0-9_])',          'SIZE'),
-        (r'(?![A-Za-z0-9_])dword(?![A-Za-z0-9_])',          'SIZE'),
-        (r'(?![A-Za-z0-9_])word(?![A-Za-z0-9_])',           'SIZE'),
-        (r'(?![A-Za-z0-9_])byte(?![A-Za-z0-9_])',           'SIZE'),
-        (r'[0-9]+',                                         'INT'),
-        (r'[_A-Za-z][A-Za-z0-9_]*',                         'ID')
+        (r'\n',                                             None),
+        (r'[ \t]+',                                         None),
+        (r'#[^\n]*',                                        None),
+        (r'\[',                                             'RESERVED'),
+        (r'\]',                                             'RESERVED'),
+        (r'\:',                                             'RESERVED'),
+        (r'\;',                                             'RESERVED'),
+        (r',',                                              'RESERVED'),
+        (r'\(',                                             'RESERVED'),
+        (r'\)',                                             'RESERVED'),
+        (r'\{',                                             'RESERVED'),
+        (r'\}',                                             'RESERVED'),
+        (r'@',                                              'UNARY_OPERATOR'),
+        (r'\$',                                             'UNARY_OPERATOR'),
+        (r'!',                                              'UNARY_OPERATOR'),
+        (r'==',                                             'RELATIONAL_OPERATOR'),
+        (r'<=',                                             'RELATIONAL_OPERATOR'),
+        (r'<',                                              'RELATIONAL_OPERATOR'),
+        (r'>=',                                             'RELATIONAL_OPERATOR'),
+        (r'>',                                              'RELATIONAL_OPERATOR'),
+        (r'!=',                                             'RELATIONAL_OPERATOR'),
+        (r'&&',                                             'CONDITIONAL_OPERATOR'),
+        (r'\|\|',                                           'CONDITIONAL_OPERATOR'),
+        (r'=',                                              'ASSIGNMENT_OPERATOR'),
+        (r'\+=',                                            'ASSIGNMENT_OPERATOR'),
+        (r'\-=',                                            'ASSIGNMENT_OPERATOR'),
+        (r'\/=',                                            'ASSIGNMENT_OPERATOR'),
+        (r'\*=',                                            'ASSIGNMENT_OPERATOR'),
+        (r'\+',                                             'ARITHMETICAL_OPERATOR'),
+        (r'-',                                              'ARITHMETICAL_OPERATOR'),
+        (r'\*',                                             'ARITHMETICAL_OPERATOR'),
+        (r'/',                                              'ARITHMETICAL_OPERATOR'),
+        (r'(?<![A-Za-z0-9_])type(?![A-Za-z0-9_])',          'RESERVED'),
+        (r'(?<![A-Za-z0-9_])func(?![A-Za-z0-9_])',          'RESERVED'),
+        (r'(?<![A-Za-z0-9_])if(?![A-Za-z0-9_])',            'RESERVED'),
+        (r'(?<![A-Za-z0-9_])elif(?![A-Za-z0-9_])',          'RESERVED'),
+        (r'(?<![A-Za-z0-9_])else(?![A-Za-z0-9_])',          'RESERVED'),
+        (r'(?<![A-Za-z0-9_])while(?![A-Za-z0-9_])',         'RESERVED'),
+        (r'(?<![A-Za-z0-9_])for(?![A-Za-z0-9_])',           'RESERVED'),
+        (r'(?<![A-Za-z0-9_])break(?![A-Za-z0-9_])',         'RESERVED'),
+        (r'(?<![A-Za-z0-9_])continue(?![A-Za-z0-9_])',      'RESERVED'),
+        (r'(?<![A-Za-z0-9_])return(?![A-Za-z0-9_])',        'RESERVED'),
+        (r'\".*?\"',                                        'STRING'),
+        (r'[0-9]+\.[0-9]+',                                 'FLOAT'),
+        (r'\'.*?\'',                                        'CHAR'),
+        (r"""
+          (?<![A-Za-z0-9_])true(?![A-Za-z0-9_])|
+          (?<![A-Za-z0-9_])false(?![A-Za-z0-9_])
+          """,                                               'BOOL'),
+        (r'(?<![A-Za-z0-9_])qword(?![A-Za-z0-9_])',          'SIZE'),
+        (r'(?<![A-Za-z0-9_])dword(?![A-Za-z0-9_])',          'SIZE'),
+        (r'(?<![A-Za-z0-9_])word(?![A-Za-z0-9_])',           'SIZE'),
+        (r'(?<![A-Za-z0-9_])byte(?![A-Za-z0-9_])',           'SIZE'),
+        (r'[0-9]+',                                          'INT'),
+        (r'[_A-Za-z][A-Za-z0-9_]*',                          'ID')
     ]
 
     return lexer.lex(input, token_exprs)
@@ -69,15 +71,15 @@ if len(sys.argv) > 1:
     print("compiling", sys.argv[1])
     code = preprocessor.preprocess(f.read())
     tokens = init_tokens(code)
-    parser = parser.parse(tokens)
-    if res.is_error:
+    Parser = parser.parse(tokens)
+    if Parser.is_error:
         print("compilation canceled due to previous error(s)")
     else:
-        analyzer = analyzer.analyze(parser.get_output())
-        if res.is_error:
+        Analyzer = analyzer.analyze(Parser.get_output())
+        if Analyzer.is_error:
             print("compilation canceled due to previous error(s)")
         else:
-            generator.generate(parser)
+            generator.generate(Parser.get_output())
             print("compilation completed with no error")
 else:
     code = input("@>")
